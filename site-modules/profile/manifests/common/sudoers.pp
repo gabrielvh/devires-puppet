355ac97bb57a8bc8fa65aa440356fbdbdf7d8b2d
Gilberto Augusto Thomaz de Aquino Holms <gilberto.holms-trc@multiplus.corp>
2017-11-03 15:10:46 -0200

    Ajustes sudoers

--- site-modules/profile/manifests/common/sudoers.pp
+++ site-modules/profile/manifests/common/sudoers.pp
@@ -5,7 +5,7 @@ class profile::common::sudoers {
   include sudo
   include sudo::configs
 
-  sudo::conf { 'multiplus' :
+  sudo::conf { 'default' :
     source      => "puppet:///modules/profile/sudoers.dat",
     priority    => 0,
     require     => Class['sudo'],
