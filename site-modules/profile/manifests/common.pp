44ccff82798136bc3dad749ff091f1d810c73520
Gilberto Augusto Thomaz de Aquino Holms <gilberto.holms-trc@multiplus.corp>
2017-11-03 15:28:43 -0200

    Add resolv

--- site-modules/profile/manifests/common.pp
+++ site-modules/profile/manifests/common.pp
@@ -1,8 +1,12 @@
 class profile::common {
   
-  contain profile::common::packages
-  contain profile::common::time
-  contain profile::common::users
-  contain profile::common::sudoers
+  contain 'profile::common::packages'
+  contain 'profile::common::time'
+  contain 'profile::common::users'
+  contain 'profile::common::sudoers'
+
+  if ($::environment != 'vagrant') {  
+    contain 'profile::common::resolv'
+  }
   
 }
